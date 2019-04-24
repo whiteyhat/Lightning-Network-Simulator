6bd942b08b1d246a561f77b9b3322d4fdad6a75c
Carlos Roldan <carlosroldan26396@gmail.com>
2019-04-24 12:46:15 +0200

    Update SimulationToolTests.java

--- Simulation/src/test/java/com/carlos/lnsim/lnsim/SimulationToolTests.java
+++ Simulation/src/test/java/com/carlos/lnsim/lnsim/SimulationToolTests.java
@@ -43,21 +43,21 @@ public class SimulationToolTests {
 		System.out.println("Network Map created");
 		network.createNetworkMap();
 		System.out.println("Loading network map into the simulation");
-		Load load = new Load();
+		DataFetcher dataFetcher = new DataFetcher();
 		System.out.println("Network loaded into the simulation");
-		assertEquals(40, load.getNodes().size());
-		assertEquals(3*40, load.getChannels().size());
+		assertEquals(40, dataFetcher.getNodes().size());
+		assertEquals(3*40, dataFetcher.getChannels().size());
 	}
 
 	@Test
 	public void displayNetwork() {
-		Load load = null;
+		DataFetcher dataFetcher = null;
 		System.out.println("Creating network map using the network data model syntax");
-		NetworkMapGenerator network = new NetworkMapGenerator(40, 3, load);
+		NetworkMapGenerator network = new NetworkMapGenerator(40, 3, dataFetcher);
 		System.out.println("Network Map created");
 		network.createNetworkMap();
 		System.out.println("Loading network map into the simulation");
-		load = new Load();
+		dataFetcher = new DataFetcher();
 		System.out.println("Network loaded into the simulation");
 
 		System.out.println("Displaying simulation");
